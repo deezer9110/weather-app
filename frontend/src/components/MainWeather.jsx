394ad683f5dbee34958a2c9baf446fd5de529c72
Oliver Davis <olliedavis2099@gmail.com>
2025-07-22 18:21:24 +0100

    Created main weather display html

---
+++ frontend/src/components/MainWeather.jsx
@@ -0,0 +1,22 @@
+function MainWeather({ weather }) {
+  return (
+    <div>
+      <div className="weather-details">
+        <div className="weather-icon">
+          <img src={weather.icon} alt={weather.type} />
+        </div>
+        <div>
+          <h3>{weather.type}</h3>
+          <h3>{weather.temp}</h3>
+          <p>Precipitation: {weather.prec}</p>
+          <p>Humidity: {weather.humidity}%</p>
+          <p>Wind Speed: {weather.windSpeed}kmh</p>
+          <p>UV: {weather.UV}</p>
+        </div>
+      </div>
+      <div className="temp-graph"></div>
+    </div>
+  );
+}
+
+export default MainWeather;
