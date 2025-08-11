91a0d6e45dbc2dca1461b4ee0a0109d21cc0bd5f
Oliver <olliedavis2099@gmail.com>
2025-08-11 19:20:19 +0100

    fixed date display in nodes

--- frontend/src/components/WeatherNode.jsx
+++ frontend/src/components/WeatherNode.jsx
@@ -1,11 +1,14 @@
 import "../css/WeatherNode.css";
+import { getDate } from "../services/generalApi";
 
 function WeatherNode({ day }) {
+
+  const dateString = day ? getDate(day.dt) : "";
 
   return (
     <div className="node-weather-details">
       <div className="weather-icon node-weather-col">
-        <h3>{day.dt}</h3>
+        <h3>{dateString}</h3>
         <img
           src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
           alt={day.weather.main}
@@ -14,7 +17,7 @@ function WeatherNode({ day }) {
       <div className="node-weather-info node-weather-col">
         <h4>{day.weather[0].main}</h4>
         <h4>{day.temp.day}°C</h4>
-        <h5>Precipitation: {day.pop}%</h5>
+        <h5>Precipitation: {day.pop * 100}%</h5>
         <h5>UV: {day.uvi}</h5>
         <h5>Wind: {day.wind_speed}kmh</h5>
         <h5>Humidity: {day.humidity}%</h5>
