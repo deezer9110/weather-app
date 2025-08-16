580df551c8ef1c15fd27e4184f87076cf0343f6f
Oliver Davis <olliedavis2099@gmail.com>
2025-08-16 18:23:25 +0100

    working graph

--- frontend/src/components/TempGraph.jsx
+++ frontend/src/components/TempGraph.jsx
@@ -1,19 +1,46 @@
-import { LineChart, Line, XAxis, YAxis, Bar } from "recharts"
+import { LineChart, Line, XAxis, YAxis, Bar, Cell } from "recharts";
 import { useWeatherContext } from "../contexts/WeatherContext";
 import "../css/TempGraph.css";
 
-function TempGraph() {
+function uvBarColour(uvi) {
+  /* Colours:
+  0-3 Green: #97D700
+  3-6 Yellow: #FCE300
+  6-8 Orange: #FF8200
+  8-11 Red: #EF3340
+  11+ Purple: #9063CD
+  */
 
+  if (uvi < 3) {
+    return "#97D700";
+  } else if (uvi < 6) {
+    return "#FCE300";
+  } else if (uvi < 8) {
+    return "#FF8200";
+  } else if (uvi < 11) {
+    return "#EF3340";
+  } else {
+    return "#9063CD";
+  }
+}
+
+function TempGraph() {
   const { hours } = useWeatherContext();
 
-  return <div className="temp-graph">
-    <LineChart width={600} height={500} data={hours}> 
-      <Line type="monotone" dataKey="temp" stroke="#8884d8" />
-      <Bar dataKey="uvi" />
-      <XAxis dataKey="dt" />
-      <YAxis dataKey="temp" />
-    </LineChart>
-  </div>;
+  return (
+    <div className="temp-graph">
+      <LineChart width={hours.length * 80} height={500} data={hours}>
+        <Bar dataKey={() => 28} opacity={0.4}>
+          {hours.map((hour) => (
+            <Cell key={hour.dt} fill={uvBarColour(hour.uvi)} />
+          ))}
+        </Bar>
+        <Line type="monotone" dataKey="temp" stroke="yellow" />
+        <XAxis dataKey="dt" />
+        <YAxis dataKey="temp" />
+      </LineChart>
+    </div>
+  );
 }
 
 export default TempGraph;
